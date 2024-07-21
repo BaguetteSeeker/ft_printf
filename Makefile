af565922e9cf78ac1c80dcc1f76e19bc63a65cda
epinaud <enzo.pinaud+42@protonmail.com>
2024-07-21 22:57:30 +0200

    Polished ftparsedirs & ftparselen

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: epinaud <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/25 16:30:14 by epinaud           #+#    #+#              #
-#    Updated: 2024/07/19 20:27:30 by epinaud          ###   ########.fr        #
+#    Updated: 2024/07/21 19:57:26 by epinaud          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,7 +22,7 @@ LIBFTPTH = $(LIBFTDIR)/$(LIBFT_NAME)
 
 LIBFILESLOC = $(addprefix $(LIBFTDIR)/, $(FT_FILES))
 
-FT_FILES = ft_strlen.c ft_nbrlen.c ft_strchr.c ft_atoi.c ft_putchar_fd.c ft_putstr_fd.c ft_putnbr_fd.c ft_putnbr_base_fd.c ft_check_dup.c
+FT_FILES = ft_strlen.c ft_nbrlen.c ft_strchr.c ft_atoi.c ft_isdigit.c ft_putchar_fd.c ft_putstr_fd.c ft_putnbr_fd.c ft_putnbr_base_fd.c ft_check_dup.c
 
 LIB_OBJ = $(FT_FILES:.c=.o)
 
--- ft_flag_directives.c
+++ ft_flag_directives.c
@@ -6,11 +6,41 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 21:22:55 by epinaud           #+#    #+#             */
-/*   Updated: 2024/07/20 03:26:30 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/07/21 20:40:20 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+t_directives	ft_init_directives(t_directives dirs)
+{
+	dirs.spec = 0;
+	dirs.width = 0;
+	dirs.left = 0;
+	dirs.zero = 0;
+	dirs.star = 0;
+	dirs.precision = 0;
+	dirs.hash = 0;
+	dirs.space = 0;
+	dirs.plus = 0;
+	dirs.offset = 0;
+	return (dirs);
+}
+
+static t_directives	ft_parse_flags(const char *str, t_directives dirs)
+{
+	if (*str == '-')
+		dirs.left = '1';
+	else if (*str == '#')
+		dirs.hash = 1;
+	else if (*str == ' ')
+		dirs.space = 1;
+	else if (*str == '+')
+		dirs.plus = 1;
+	else if (*str == '0' && dirs.width == 0)
+		dirs.zero = 1;
+	return (dirs);
+}
 
 void	ft_print_dirs(t_directives dirs)
 {
@@ -43,23 +73,7 @@ void	ft_print_dirs(t_directives dirs)
 	*/
 	//check
 }
-
-t_directives	ft_init_directives(t_directives dirs)
-{
-	dirs.spec = 0;
-	dirs.width = 0;
-	dirs.left = 0;
-	dirs.zero = 0;
-	dirs.star = 0;
-	dirs.precision = 0;
-	dirs.hash = 0;
-	dirs.space = 0;
-	dirs.plus = 0;
-	dirs.offset = 0;
-	return (dirs);
-}
-
-static t_directives	ft_set_type(const char *str, t_directives dirs)
+/*static t_directives	ft_set_type(const char *str, t_directives dirs)
 {
 	while (ft_strchr("cspuidxX%", *str))
 	{
@@ -68,33 +82,32 @@ static t_directives	ft_set_type(const char *str, t_directives dirs)
 	}
 
 	return (dirs);
+}*/
+
+static int	ft_parse_len(const char *str, va_list args, int offset, int *ldest)
+{
+	if (str[offset] == '*')
+	{
+		offset++;
+		*ldest = va_arg(args, int);
+	}
+	else
+		while (ft_isdigit(str[offset]))
+			*ldest = (*ldest * 10) + (str[offset++] - '0');
+	return (offset);
 }
 
-t_directives	ft_parse_directives(const char *str, va_list args, t_directives dirs)
+t_directives	ft_parse_dirs(const char *str, va_list args, t_directives dirs)
 {
-	while (*str && ft_strchr("-0.*# +", *str))
-	{
-		if (*str == '-')
-			dirs.left = '1';
-		else if (*str == '#')
-			dirs.hash = 1;
-		else if (*str == ' ')
-			dirs.space = 1;
-		else if (*str == '+')
-			dirs.plus = 1;
-		else if (*str == '0' && dirs.width == 0)
-			dirs.zero = 1;
-		else if (*str == '*')
-			dirs.width = va_arg(args, int);			
-		else if (*str == '.' && str[1] == '*')
-			dirs.precision = va_arg(args, int);
-		else if (*str >= '0' && *str <= '9' && !dirs.precision)
-			dirs.width = (dirs.width * 10) + (*str - '0');
-		else if (*str >= '0' && *str <= '9' && dirs.precision)
-			dirs.precision = (dirs.precision * 10) + (*str - '0');
+	while (ft_strchr("-#+ 0", str[dirs.offset]))
+		dirs = ft_parse_flags(&str[dirs.offset++], dirs);
+	dirs.offset += ft_parse_len(str, args, dirs.offset, &dirs.width);
+	if (str[dirs.offset] == '.' )
+	{	
 		dirs.offset++;
-		if (ft_strchr("cspuidxX%", *str++))
-			return (ft_set_type(str, dirs));
+		dirs.offset += ft_parse_len(str, args, dirs.offset, &dirs.precision);
 	}
+	//while (ft_strchr("cspuidxX%", str[dirs.offset++]))
+		//dirs = ft_parse_spec();
 	return (dirs);
 }
